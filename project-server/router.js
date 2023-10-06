b1d9917575554dfd607c3d57801afb380c94cbd3
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-10-06 15:55:37 -0500

    removed process.env

--- project-server/router.js
+++ project-server/router.js
@@ -5,4 +5,19 @@ const express = require("express");
 // Create router
 const router = express.Router();
 // ...
+const API = "api";
+// City search suggestions
+router.get(`/${API}/search`, async (req, res) => {
+  const { keyword } = req.query;
+  const response = await amadeus.referenceData.locations.get({
+    keyword,
+    subType: Amadeus.location.city,
+  });
+  try {
+    await res.json(JSON.parse(response.body));
+  } catch (err) {
+    await res.json(err);
+  }
+});
+
 module.exports = router;
--- project-server/server.js
+++ project-server/server.js
@@ -9,29 +9,6 @@ app.use(express.json());
 // Apply router
 app.use("/", router);
 
-// Define a route for making the Amadeus API call
-// app.post("/amadeus-api", async (req, res) => {
-//   try {
-//     const { client_id, client_secret } = req.body; // Your Amadeus API credentials
-
-//     // Make a POST request to the Amadeus API
-//     const response = await axios.post(
-//       "https://test.api.amadeus.com/v1/security/oauth2/token",
-//       {
-//         grant_type: "client_credentials",
-//         client_id,
-//         client_secret,
-//       }
-//     );
-
-//     // Handle the API response here
-//     res.json(response.data);
-//   } catch (error) {
-//     // Handle errors
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
