32d202a6191c5c3dd3ce54b174aad65b65652642
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-10-05 22:54:04 -0500

    components

---
+++ project-client/src/components/Home.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Home</h1>
+    </div>
+  );
+}
---
+++ project-client/src/components/NavBar.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return <nav className="navbar"></nav>;
+}
