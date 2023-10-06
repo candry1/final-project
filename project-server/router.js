edbd93c24dcbb6b093586206881194a97b3cbfe9
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-10-06 00:01:50 -0500

    added env variables

---
+++ project-server/router.js
@@ -0,0 +1,8 @@
+// router.js
+const { API_KEY, API_SECRET } = require("./config");
+const Amadeus = require("amadeus");
+const express = require("express");
+// Create router
+const router = express.Router();
+// ...
+module.exports = router;
--- project-server/server.js
+++ project-server/server.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
+const router = require("./router");
 const port = process.env.PORT || 6000;
 
 app.use(express.json());
 
 // Define your routes and API endpoints here
+// Apply router
+app.use("/", router);
+
+// Define a route for making the Amadeus API call
+// app.post("/amadeus-api", async (req, res) => {
+//   try {
+//     const { client_id, client_secret } = req.body; // Your Amadeus API credentials
+
+//     // Make a POST request to the Amadeus API
+//     const response = await axios.post(
+//       "https://test.api.amadeus.com/v1/security/oauth2/token",
+//       {
+//         grant_type: "client_credentials",
+//         client_id,
+//         client_secret,
+//       }
+//     );
+
+//     // Handle the API response here
+//     res.json(response.data);
+//   } catch (error) {
+//     // Handle errors
+//     res.status(500).json({ error: "An error occurred" });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
