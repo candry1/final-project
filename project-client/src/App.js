fd26df808d6597e45e5473f59aaf316884983571
Charlotte Andry <charlotteandry123@gmail.com>
2023-10-05 23:58:23 -0500

    basic DRAFTED PAGE for input

--- project-client/src/App.js
+++ project-client/src/App.js
@@ -1,17 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Home from "./components/Home";
+import SubmissionForm from "./components/Submission";
+import { useState } from "react";
 
 function App() {
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [groupSize, setGroupSize] = useState(0);
+  const [travelDates, setTravelDates] = useState("");
+
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
         <Navbar />
-        <Routes>
+        <Home />
+        <SubmissionForm />
+        {/* <Routes>
           <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+        </Routes> */}
+      {/* </BrowserRouter> */}
     </div>
   );
 }
--- project-client/src/components/Home.js
+++ project-client/src/components/Home.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import "./Home.css";
 
-export default function Home() {
+function Home() {
   return (
     <div className="Home">
       <h1>Home</h1>
     </div>
   );
 }
+
+export default Home;
