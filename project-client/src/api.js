72093b2fefdc1898f04681bcbe175e656a8398cc
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-10-17 22:51:04 -0500

    axios

---
+++ project-client/src/api.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const search = (input) => {
+  if (input) {
+    try {
+      const request = axios.get(`/api/search?keyword=${input}`);
+      return {
+        async process(callback) {
+          request.then((response) => {
+            const json = response.data;
+            if (json && json.data) {
+              callback(
+                json.data.map(({ address }) => {
+                  return {
+                    city: address.cityName,
+                    code: address.cityCode,
+                    country: address.countryName,
+                    state: address.stateCode,
+                  };
+                })
+              );
+            }
+          });
+        },
+        cancel() {
+          source.cancel();
+        },
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return {
+    process() {
+      return [];
+    },
+    cancel() {},
+  };
+};
