b39fa4c3821550c37efbe484d838589169e59c34
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-11-13 17:32:55 -0600

    api

--- project-server/amadeus.js
+++ project-server/amadeus.js
@@ -3,7 +3,7 @@ const Amadeus = require("amadeus");
 const amadeus = new Amadeus({ clientId: API_KEY, clientSecret: API_SECRET }); // Initialize Amadeus
 
 const hotelIds = "ALCHI347";
-
+const adults = 2;
 (async () => {
   const response = await amadeus.shopping.hotelOffersSearch.get({
     hotelIds,
