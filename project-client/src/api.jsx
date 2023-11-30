1d9e0268a08e29f00688321012e242a637d0c358
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-11-29 19:57:42 -0600

    base url

--- project-client/src/api.jsx
+++ project-client/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://getawayguide123.onrender.com/";
 const search = (input) => {
   if (input) {
     try {
